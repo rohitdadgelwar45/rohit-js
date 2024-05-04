e9a5193e2b3818ecff1fb6f76c170a3acc65046a
Rohit45 <120206926+rohitdadgelwar45@users.noreply.github.com>
2024-05-04 17:22:24 +0000

    datatypes complete

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let age=21;
+let name="rohit";
+let my_name_is_rohit=true;
+// null => standalone value;
+// undefined=> value not assign
+// symbol=>characters/ unique
+
+// object=>
+
+    console.log(typeof(age));
+    console.log(typeof(name));
+    console.log(typeof(my_name_is_rohit));
+    console.log("this is for check");
+    console.log(typeof(undefined));
+    console.log(typeof(null));// object is the type of null
